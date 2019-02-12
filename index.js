264197c1c3395d52b09a5ba231b0f8f585605ad6
iammadab <wisdom_ogwu@yahoo.com>
2019-02-12 14:49:55 +0100

    extraction of options and flags enabled

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const parser = require("./lib/parser")
 
-parser.use({
-	commands: {
-		login: ["-u", "-p"]
-	},
-	options: {
-		username: "-u",
-		password: "-p"
-	},
-	flags: {
-		username: "-u"
-	}
-})
+// parser.use({
+// 	commands: {
+// 		login: ["-u", "-p"]
+// 	},
+// 	options: {
+// 		username: "-u",
+// 		password: "-p"
+// 	},
+// 	flags: {
+// 		username: "-u"
+// 	}
+// })
 
-parser.parse(process.argv)
+// parser.parse(process.argv)
 
 
 module.exports = parser
