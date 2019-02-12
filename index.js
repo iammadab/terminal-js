e74e39840ab7257ba2a03ed6a435d18287940a66
iammadab <wisdom_ogwu@yahoo.com>
2019-02-12 13:56:18 +0100

    Validating for clashing names in config object

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const parser = require("./lib/parser")
 
+parser.use({
+	commands: {
+		login: ["-u", "-p"]
+	},
+	options: {
+		username: "-u",
+		password: "-p"
+	},
+	flags: {
+		username: "-u"
+	}
+})
+
+parser.parse(process.argv)
+
+
 module.exports = parser
