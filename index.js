ff82b2bc8b0b932bc7cf5cab22ee058c31d2b9e6
iammadab <wisdom_ogwu@yahoo.com>
2019-02-16 16:31:52 +0100

    renamed project from cli to terminal js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 const parser = require("./lib/parser")
 
-// parser.use({
-// 	commands: {
-// 		push: ["remote", "login"],
-// 		login: ["-u", "-p"]
-// 	},
-// 	options: {
-// 		username: "-u",
-// 		password: "-p"
-// 	},
-// 	flags: {
-// 		verbose: "-v"
-// 	}
-// })
-
-// parser.parse(process.argv)
-
-
 module.exports = parser
